5b3d3f83c3222a069d8eddaac27a94dcf89d9506
Daniel Montes Gil <dmontesgil@gmail.com>
2024-11-03 15:38:31 +0100

    Main UI and Health Bar classes created

---
+++ UnrealBootcamp/Source/UnrealBootcamp/Scripts/UBHealthBar.h
@@ -0,0 +1,16 @@
+// © 2024 Daniel Montes Gil - dmontesgil@gmail.comAll rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UBUserWidget.h"
+#include "UBHealthBar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALBOOTCAMP_API UUBHealthBar : public UUBUserWidget
+{
+	GENERATED_BODY()
+};
---
+++ UnrealBootcamp/Source/UnrealBootcamp/Scripts/UBMainUI.cpp
@@ -0,0 +1,4 @@
+// © 2024 Daniel Montes Gil - dmontesgil@gmail.comAll rights reserved.
+
+
+#include "UBMainUI.h"
