b9f310b2aed51ac6ebc4d5806471397c082fc6d9
Daniel Montes Gil <dmontesgil@gmail.com>
2024-11-04 07:41:07 +0100

    Added SimpleText

---
+++ UnrealBootcamp/Source/UnrealBootcamp/Scripts/BUSimpleText.h
@@ -0,0 +1,16 @@
+// © 2024 Daniel Montes Gil - dmontesgil@gmail.com All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UBUserWidget.h"
+#include "BUSimpleText.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALBOOTCAMP_API UBUSimpleText : public UUBUserWidget
+{
+	GENERATED_BODY()
+};
