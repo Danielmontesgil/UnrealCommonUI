f21d684fab1bd98a2a61c2d4906ba3dcf450907d
Daniel Montes Gil <dmontesgil@gmail.com>
2024-11-16 13:24:15 +0100

    MainTopBard improved and blueprint updated

--- UnrealBootcamp/Source/UnrealBootcamp/Scripts/UBMainTopBar.h
+++ UnrealBootcamp/Source/UnrealBootcamp/Scripts/UBMainTopBar.h
@@ -15,34 +15,34 @@ class UNREALBOOTCAMP_API UUBMainTopBar : public UUBUserWidget
 {
 	GENERATED_BODY()
 
-protected:
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "UI")
+public:
+	UPROPERTY(BlueprintReadWrite, Category = "UI")
 	UUBSimpleText* TimerLabelText;
 
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "UI", meta = (BindWidget))
+	UPROPERTY(BlueprintReadWrite, Category = "UI", meta = (BindWidget))
 	UUBSimpleText* TimerText;
 
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "UI")
+	UPROPERTY(BlueprintReadWrite, Category = "UI")
 	UUBSimpleText* KillsLabelText;
 
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "UI", meta = (BindWidget))
+	UPROPERTY(BlueprintReadWrite, Category = "UI", meta = (BindWidget))
 	UUBSimpleText* KillsText;
 
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "UI")
+	UPROPERTY(BlueprintReadWrite, Category = "UI")
 	UUBSimpleText* EnemyKillsLabelText;
 
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "UI", meta = (BindWidget))
+	UPROPERTY(BlueprintReadWrite, Category = "UI", meta = (BindWidget))
 	UUBSimpleText* EnemyKillsText;
 
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "UI")
+	UPROPERTY(BlueprintReadWrite, Category = "UI")
 	UUBSimpleText* PointsLabelText;
 
-	UPROPERTY(blueprintReadWrite, EditAnywhere, Category = "UI", meta = (BindWidget))
+	UPROPERTY(blueprintReadWrite, Category = "UI", meta = (BindWidget))
 	UUBSimpleText* PointsText;
 
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "UI")
+	UPROPERTY(BlueprintReadWrite, Category = "UI")
 	UUBSimpleText* EnemyPointsLabelText;
 
-	UPROPERTY(BlueprintReadWrite, editAnywhere, Category = "UI", meta = (BindWidget))
+	UPROPERTY(BlueprintReadWrite, Category = "UI", meta = (BindWidget))
 	UUBSimpleText* EnemyPointsText;
 };
