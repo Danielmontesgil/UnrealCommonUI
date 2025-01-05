48f0a6bcccf08e0627003216fb13fe9d5fbd0ea7
Daniel Montes Gil <dmontesgil@gmail.com>
2025-01-05 11:19:57 +0100

    Generic Popup class and view

---
+++ Source/Stealth/MainMenu/View/GenericPopUpView.h
@@ -0,0 +1,28 @@
+// © 2024 Daniel Montes Gil - dmontesgil@gmail.com All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "GenericPopUpView.generated.h"
+
+class UCommonTextBlock;
+class UStealthCommonButton;
+/**
+ * 
+ */
+UCLASS()
+class STEALTH_API UGenericPopUpView : public UCommonActivatableWidget
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	UStealthCommonButton* OkButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UStealthCommonButton* CancelButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UCommonTextBlock* PopUpText;
+};
