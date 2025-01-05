fbd99f367e019f739e952022460fe6d17f71f88b
Daniel Montes Gil <dmontesgil@gmail.com>
2025-01-05 10:12:30 +0100

    GameUI moved to backlog

--- GameUI/Source/GameUI/GameUI.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GameUI.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameUI, "GameUI" );
-
-DEFINE_LOG_CATEGORY(LogGameUI)
- 
--- GameUI/Source/GameUI/Scripts/GameHUD.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "GameHUD.h"
-
-UGameHUD::UGameHUD(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
-{
-}
