044fe8ec9fd3216aa3329824c7ac42f39837a2e9
Daniel Montes Gil <dmontesgil@gmail.com>
2025-01-05 10:11:41 +0100

    CommonUI moved to backlog

--- CommonUIv1/Source/CommonUIv1/CommonUIv1.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CommonUIv1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CommonUIv1, "CommonUIv1" );
-
-DEFINE_LOG_CATEGORY(LogCommonUIv1)
- 
